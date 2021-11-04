7e95a2b4ed16727ec4ca28e91944847772350ca4
Fy5tew <75914234+Fy5tew@users.noreply.github.com>
2021-11-04 20:38:32 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,57 @@
+const teamleads = [
+	"? Олег Михайлович",
+	"? Арина Егоровна",
+	"? Дарья Александровна",
+]
 
+const students = [
+	"Круглик Максим Сергеевич",
+	"Радюк Евгений Александрович",
+	"Мацкевич Марк Николаевич",
+	"Янукович Евгений Дмитриевич",
+	"Кляшторный Вячеслав Андреевич",
+	"Батук Денис Сергеевич",
+	"Тарасенко Давид Игоревич",
+	"Койфман Давид Александрович",
+	"Ложечка Артём Александрович",
+	"Слинка Егор Игоревич",
+]
+
+
+
+
+
+function insertTeamleadCards() {
+	for (let i = 0; i < teamleads.length; i++) {
+		let card = `
+<div class="teamlead_card">
+	<li>
+		<div class="img_container">
+			<img src="images/teamlead${i+1}.jpg">
+		</div>
+		<h3 class="teamlead_name">${teamleads[i]}</h3>
+	</li>
+</div>
+		`;
+		document.querySelector(".teamlead_list").insertAdjacentHTML("beforeEnd", card);
+	}
+}
+
+function insertStudentCards() {
+	for (let i = 0; i < students.length; i++) {
+		let card = `
+<div class="student_card">
+	<li>
+		<div class="img_container">
+			<img src="images/student${i+1}.jpg">
+		</div>
+		<h3 class="student_name">${students[i]}</h3>
+	</li>
+</div>		
+		`
+		document.querySelector(".student_list").insertAdjacentHTML("beforeEnd", card);
+	}
+}
+
+insertTeamleadCards();
+insertStudentCards();

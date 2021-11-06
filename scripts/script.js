fcc4e75c54675fbc9c17fdbbd752f7b432cedc34
Fy5tew <75914234+Fy5tew@users.noreply.github.com>
2021-11-06 12:09:36 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,66 @@ const teamleads = [
 ]
 
 const students = [
-	"Круглик Максим Сергеевич",
-	"Радюк Евгений Александрович",
-	"Мацкевич Марк Николаевич",
-	"Янукович Евгений Дмитриевич",
-	"Кляшторный Вячеслав Андреевич",
-	"Батук Денис Сергеевич",
-	"Тарасенко Давид Игоревич",
-	"Койфман Давид Александрович",
-	"Ложечка Артём Александрович",
-	"Слинка Егор Игоревич",
+	{
+        name: "Круглик Максим Сергеевич",
+        photo: "student1.jpg",
+        links: [
+        	"https://www.instagram.com/kruglik.max",
+        ],
+	},
+	{
+        name: "Радюк Евгений Александрович",
+        photo: "student2.jpg",
+        links: [
+        	"https://www.instagram.com/_zh_rad_",
+        ],
+	},
+	{
+        name: "Мацкевич Марк Николаевич",
+        photo: "student3.jpg",
+        links: [],
+	},
+	{
+        name: "Янукович Евгений Дмитриевич",
+        photo: "student4.jpg",
+        links: [
+        	"https://www.vk.com/evyanukovich",
+        ],
+	},
+	{
+        name: "Кляшторный Вячеслав Андреевич",
+        photo: "student5.jpg",
+        links: [
+        	"https://www.instagram.com/shtornyy",
+        ],
+	},
+	{
+        name: "Батук Денис Сергеевич",
+        photo: "student6.jpg",
+        links: [
+        	"https://www.instagram.com/_diu_410_",
+        ],
+	},
+	{
+        name: "Тарасенко Давид Игоревич",
+        photo: "student7.jpg",
+        links: [],
+	},
+	{
+        name: "Койфман Давид Александрович",
+        photo: "student8.jpg",
+        links: [],
+	},
+	{
+        name: "Ложечка Артём Александрович",
+        photo: "student9.jpg",
+        links: [],
+	},
+	{
+        name: "Слинка Егор Игоревич",
+        photo: "student10.jpg",
+        links: [],
+	},
 ]
 
 
@@ -27,7 +77,7 @@ function insertTeamleadCards() {
 <div class="teamlead_card">
 	<li>
 		<div class="img_container">
-			<img src="images/teamlead${i+1}.jpg">
+			<img src="images/teamleads/teamlead${i+1}.jpg">
 		</div>
 		<h3 class="teamlead_name">${teamleads[i]}</h3>
 	</li>
@@ -39,13 +89,32 @@ function insertTeamleadCards() {
 
 function insertStudentCards() {
 	for (let i = 0; i < students.length; i++) {
+		let student = students[i];
+
+		let links = "";
+		for (let j in student.links) {
+			let link = student.links[j];
+			let type;
+
+			if (/vk/.test(link)) {
+				type = "VK";
+			} else if (/instagram/.test(link)) {
+				type = "Instagram";
+			} else {
+				continue;
+			}
+
+			links += `<a class="link" href="${link}" target="_blank"><img src="images/logo/${type}.png"></a>`;
+		}
+
 		let card = `
 <div class="student_card">
 	<li>
 		<div class="img_container">
-			<img src="images/student${i+1}.jpg">
+			<img src="images/students/${student.photo}">
 		</div>
-		<h3 class="student_name">${students[i]}</h3>
+		<h3 class="student_name">${student.name}</h3>
+		${links}
 	</li>
 </div>		
 		`
